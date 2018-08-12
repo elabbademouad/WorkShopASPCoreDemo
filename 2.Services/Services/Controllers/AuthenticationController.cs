3002b68e0bcdae6c4afe68470fa6a798f5a45ff0
unknown <elabbademouad@gmail.com>
2018-08-12 21:53:11 +0100

    a

--- 2.Services/Services/Controllers/AuthenticationController.cs
+++ 2.Services/Services/Controllers/AuthenticationController.cs
@@ -1,9 +1,12 @@
 using Microsoft.AspNetCore.Mvc;
 namespace Services.Controllers
-{
+{   
+    [Route("api/[controller]")]
     public class AuthenticationController : Controller
     {
-       
+        public IActionResult Register(){
+            return Ok("");
+        }
 
     }
 }
